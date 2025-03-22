f098110eba1a745575f6373fc20ed8bda21d0a70
Latte <koryhalovtimofei@gmail.com>
2025-03-22 19:41:31 +0200

    Updated script - calc.py was deleted, but script now in main.py

--- main.py
+++ main.py
@@ -1,17 +1,72 @@
 import os
-import apps.calc
+
+
+def clear():
+    os.system('cls')
+
+def question():
+    clear()
+    answ11 = input("input a math char: ")
+    if answ11 == "+":
+        plus()
+    elif answ11 == "-":
+        minus()
+
+def plus():
+    clear()
+    x = int(input("input num1: "))
+    y = int(input("input num2: "))
+    res = x + y
+    print("Result: ", res)
+
+def minus():
+    clear()
+    x = int(input("input num1: "))
+    y = int(input("input num2: "))
+    res = x - y
+    print("Result: ", res)
+
+
+def note():
+    clear()
+    filename = "notes.txt"
+
+    content = ""
+    try:
+        with open(filename, "r") as file:
+            content = file.read()
+    except FileNotFoundError:
+        print("File not found")
+
+    if content:
+        print(content)
+    else:
+        print("File is empty")
+
+    new_text = input("Enter new text: ").strip()
+    with open(filename, "a") as file:
+        file.write(new_text + "\n")
+
+
 
 def menu():
-    os.system('cls')
-    calc = "calc"
+    clear()
+    calc_t = "calc"
+    note_t = "note"
     
-    print("COS. alpha 0.0.0.3 (build 35)")
+    print("COS. alpha 0.0.0.4.1 (build 60)")
     print("======== MENU ========")
-    print(calc)
+    print(calc_t)
+    print(note_t)
     print("======================")
     answ = input("input a program: ")
-    if answ == calc:
-        apps.calc.plus()
+    if answ == calc_t:
+        question()
+        answ = int(input("Input '0' to close program... "))
+        if answ == 0:
+            menu()
+    elif answ == note_t:
+        note()
         answ = int(input("Input '0' to close program... "))
         if answ == 0:
             menu()
