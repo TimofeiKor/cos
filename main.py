76b688e33f27d654feb7e04d1d0487d841b1615c
Latte <koryhalovtimofei@gmail.com>
2025-03-22 18:49:27 +0200

    Added new program 'note'

--- main.py
+++ main.py
@@ -1,17 +1,46 @@
 import os
 import apps.calc
 
+def note():
+    os.system('cls')
+    filename = "notes.txt"
+
+    content = ""
+    try:
+        with open(filename, "r") as file:
+            content = file.read()
+    except FileNotFoundError:
+        print("File not found")
+
+    if content:
+        print(content)
+    else:
+        print("File is empty")
+
+    new_text = input("Enter new text: ").strip()
+    with open(filename, "a") as file:
+        file.write(new_text + "\n")
+
+
+
 def menu():
     os.system('cls')
     calc = "calc"
+    note_t = "note"
     
-    print("COS. alpha 0.0.0.3 (build 35)")
+    print("COS. alpha 0.0.0.4 (build 57)")
     print("======== MENU ========")
     print(calc)
+    print(note_t)
     print("======================")
     answ = input("input a program: ")
     if answ == calc:
         apps.calc.plus()
+        answ = int(input("Input '0' to close program... "))
+        if answ == 0:
+            menu()
+    elif answ == note_t:
+        note()
         answ = int(input("Input '0' to close program... "))
         if answ == 0:
             menu()
