abfe2fae92af01d404f1cf532451a302ad689fb9
Latte <koryhalovtimofei@gmail.com>
2025-03-23 20:15:47 +0200

    COS Build 107 - updated functionality of Core: user can run avaiable apps

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
 
+calc_t = "calc"
+note_t = "note"
+cmdcore = "core"
+
 def info():
-    print("COS. alpha 0.0.0.5 (build 97)")
+    print("COS. alpha 0.0.0.5.3 (build 107)")
 
 def info_core():
-    print("COS Core. alpha 0.0.0.1 (build 10)")
+    print("COS Core. alpha 0.0.0.1.2 (build 23)")
 
 def clear():
     os.system('cls')
@@ -15,12 +19,17 @@ def core():
     cmd = input("cos/core: ")
     if cmd == "info":
         info_core()
+        core()
     elif cmd == "clear":
         clear()
         core()
     elif cmd == "0":
         menu()
-        
+    elif cmd == calc_t:
+        question()
+    elif cmd == note_t:
+        note()
+
 
 
 def question():
@@ -41,6 +50,9 @@ def plus():
     y = int(input("input num2: "))
     res = x + y
     print("Result: ", res)
+    answ = int(input("Input '0' to close program... "))
+    if answ == 0:
+        menu()
 
 def minus():
     clear()
@@ -48,6 +60,9 @@ def minus():
     y = int(input("input num2: "))
     res = x - y
     print("Result: ", res)
+    answ = int(input("Input '0' to close program... "))
+    if answ == 0:
+        menu()
 
 def multiply():
     clear()
@@ -55,6 +70,9 @@ def multiply():
     y = int(input("input num2: "))
     res = x * y
     print("Result: ", res)
+    answ = int(input("Input '0' to close program... "))
+    if answ == 0:
+        menu()
 
 def divide():
     clear()
@@ -62,6 +80,9 @@ def divide():
     y = int(input("input num2: "))
     res = x / y
     print("Result: ", res)
+    answ = int(input("Input '0' to close program... "))
+    if answ == 0:
+        menu()
 
 
 def note():
@@ -83,14 +104,14 @@ def note():
     new_text = input("Enter new text: ").strip()
     with open(filename, "a") as file:
         file.write(new_text + "\n")
+    answ = int(input("Input '0' to close program... "))
+    if answ == 0:
+        menu()
 
 
 
 def menu():
     clear()
-    calc_t = "calc"
-    note_t = "note"
-    cmdcore = "core"
     
     info()
     print("======== MENU ========")
@@ -101,14 +122,8 @@ def menu():
     answ = input("input a program: ")
     if answ == calc_t:
         question()
-        answ = int(input("Input '0' to close program... "))
-        if answ == 0:
-            menu()
     elif answ == note_t:
         note()
-        answ = int(input("Input '0' to close program... "))
-        if answ == 0:
-            menu()
     elif answ == cmdcore:
         core()
 
